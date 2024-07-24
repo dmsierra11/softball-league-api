1f13eb751f3b458179465b89efad94f650709b77
Daniel Sierra <danieluchin@gmail.com>
2024-07-25 01:46:35 +0200

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,4 +22,13 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
+  overrides: [
+    {
+      files: ['scripts/*'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+        'import/no-commonjs': 'off',
+      },
+    },
+  ],
 };
