2d29c6f475b541059d62dfc1a7b5fd2525831a31
Daniel Sierra <danieluchin@gmail.com>
2024-07-27 11:07:24 +0200

    Players

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,5 +1,68 @@
+import { Injectable, Query } from '@nestjs/common';
+import { Pool } from 'pg';
+import { Player } from './players.types';
 
-  import { Injectable } from '@nestjs/common';
+@Injectable()
+export class PlayersService {
+  private pool: Pool;
 
-  @Injectable()
-  export class playersService {}
+  constructor() {
+    this.pool = new Pool({
+      connectionString: process.env.POSTGRES_URL,
+    });
+  }
+
+  async findAll() {
+    const result = await this.pool.query('SELECT * FROM players');
+    return result.rows;
+  }
+
+  async findOne(id: number) {
+    const result = await this.pool.query(
+      'SELECT * FROM players WHERE id = $1',
+      [id],
+    );
+    return result.rows[0];
+  }
+
+  async findByName(name: string) {
+    const result = await this.pool.query(
+      'SELECT * FROM players WHERE name = $1',
+      [name],
+    );
+    return result.rows[0];
+  }
+
+  async createPlayer(player: Player) {
+    const result = await this.pool.query(
+      'INSERT INTO players (name, positions, nationality, number) VALUES ($1, $2, $3, $4) RETURNING id',
+      [player.name, player.positions, player.nationality, player.number],
+    );
+    return result.rows[0];
+  }
+
+  async updatePlayer(id: number, player: Player) {
+    if (player.name) {
+      await this.pool.query('UPDATE players SET name = $2 WHERE id = $1', [
+        id,
+        player.name,
+      ]);
+    }
+
+    if (player.number) {
+      await this.pool.query('UPDATE players SET number = $2 WHERE id = $1', [
+        id,
+        player.number,
+      ]);
+    }
+
+    if (player.positions && player.positions.length > 0) {
+      await this.pool.query('UPDATE players SET positions = $2 WHERE id = $1', [
+        id,
+        player.positions,
+      ]);
+    }
+
+    return player;
+  }
+}
--- src/players/players.types.ts
+++ src/players/players.types.ts
@@ -1,12 +1,8 @@
-export interface PlayerStats {
+export type Player = {
+  id?: string;
   name: string;
-  team_id?: string;
-  position?: string;
-  stats: {
-    [key: string]: string | number;
-  };
-}
-
-export type StatsType = {
-  [key: string]: string | number;
+  nationality?: string;
+  gender?: string;
+  number?: string;
+  positions?: string[];
 };
